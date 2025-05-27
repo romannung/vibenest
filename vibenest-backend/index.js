58aaec11a1398bf2580c9e7d6531f8fa11c3940a
Roman <roman.ivanyshyn-ip213@nung.edu.ua>
2025-05-27 11:48:01 +0300

    Changed localhost to 0.0.0.0

--- vibenest-backend/index.js
+++ vibenest-backend/index.js
@@ -40,6 +40,6 @@ app.use("/api/playlists/", playlistRouter);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, '0.0.0.0', async () => {
+app.listen(port, '0.0.0.0', () => {
 	console.log(`SERVER RUNNING ON PORT ${port}`);
 });
--- vibenest-frontend/src/api/index.js
+++ vibenest-frontend/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const client = axios.create({
-	baseURL: "http://localhost:5000/api/",
+	baseURL: "http://0.0.0.0:5000/api/",
 });
