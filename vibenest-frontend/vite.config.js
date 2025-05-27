85dd24973f885e3cadfd893a344c540244bcfc6c
Sasha <oleksandr.ambroz-ip213@nung.edu.ua>
2025-05-27 13:38:11 +0300

    Frontend fix #1

--- vibenest-frontend/vite.config.js
+++ vibenest-frontend/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true
-  }
+    proxy: {
+      '/api': {
+        target: 'https://vibenest-e7v6.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
