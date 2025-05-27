f3651f4c667c2a0352757aecceac42b6d96d9c69
Roman <roman.ivanyshyn-ip213@nung.edu.ua>
2025-05-27 13:06:14 +0300

    backend cors fix #2

--- vibenest-backend/index.js
+++ vibenest-backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-	origin: ['https://vibenest-1-0zkv.onrender.com', 'http://localhost:5173'],
+	origin: ['https://vibenest-1-0zkv.onrender.com', 'http://localhost:5173', 'http://localhost:3000'],
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 	credentials: true
 }));
@@ -40,6 +40,6 @@ app.use("/api/playlists/", playlistRouter);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', async () => {
 	console.log(`SERVER RUNNING ON PORT ${port}`);
 });
