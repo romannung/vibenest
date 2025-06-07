71e412bfecec003226d88f20137834d8a78de4db
Roman <roman.ivanyshyn-ip213@nung.edu.ua>
2025-06-07 12:08:13 +0300

    Add hosted fronted to CORS conf

--- vibenest-backend/index.js
+++ vibenest-backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-	origin: ['http://localhost:5173', 'http://localhost:3000'],
+	origin: ['https://vibenest-1-0zkv.onrender.com', 'http://localhost:5000', 'http://localhost:5173'],
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 	credentials: true
 }));
